4a385d4ed1355893de631ecdabf8fd2ceb40e8a2
zakir <zakirhossaib736@gmail.com>
2023-06-01 23:52:43 +0600

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,98 @@
 "use strict";
 
-const browsersync = require("browser-sync").create();
-const gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass')); 
+// Load plugins
 const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const cssnano = require("cssnano");
+const concat = require('gulp-concat');
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
 const minify = require('gulp-clean-css');
 const terser = require('gulp-terser');
-const imagemin = require('gulp-imagemin');
 const imagewebp = require('gulp-webp');
+var sass = require('gulp-sass')(require('sass'));
+const livereload = require('gulp-livereload');
 var reload = browsersync.reload;
 
-// == Browser-sync task
-gulp.task("browser-sync", function(done){
-    browsersync.init({
-      server: "./",
-      startPath: "./index.html",
-      //    browser: 'chrome',
-      host: 'localhost',
-      //    port: 4000,
-      open: true,
-      tunnel: true 
-    });
-    gulp.watch(["./**/*.html"]).on("change", reload);
-    done(); 
-  });
 
+
+// == Browser-sync task
+gulp.task("browser-sync", function (done) {
+  browsersync.init({
+    server: "./",
+    startPath: "/pages/index.html", 
+    //    browser: 'chrome',
+    host: 'localhost',
+       port: 4040,
+    open: true,
+    tunnel: true
+  });
+  gulp.watch(["./**/*.html"]).on("change", reload); 
+  done();
+});
 
 // CSS task
 gulp.task("css", () => {
   return gulp
-    .src("./scss/index.scss")
+    .src("assets/scss/style.scss")
     .pipe(sass({ outputStyle: "expanded" }))
-    .pipe(autoprefixer('last 2 versions'))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(minify())
-    .pipe(gulp.dest('dist/css')) // change to your final/public directory
+    .pipe(gulp.dest("dist/css"))
     .pipe(browsersync.stream())
+    .pipe(livereload())
 });
 
-//optimize and move images
-    gulp.task("optimizeimg", () => {
-        return gulp
-  .src('./images/*.{jpg,png}') // change to your source directory
-    .pipe(imagemin([
-      imagemin.mozjpeg({ quality: 80, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 2 }),
-    ]))
-    .pipe(gulp.dest('dist/images')) // change to your final/public directory
+// Webfonts task
+gulp.task("webfonts", () => {
+  return (
+  gulp
+    .src("assets/scss/vendor/webfonts/*.{ttf,woff,woff2,eot,svg}")
+    .pipe(gulp.dest('dist/css/webfonts'))
+    );
 });
 
 //optimize and move images
 gulp.task("webpImage", () => {
-        return gulp
-     .src('dist/images/*.{jpg,png}') // change to your source directory
-    .pipe(imagewebp())
-    .pipe(gulp.dest('dist/images')) // change to your final/public directory
+  return( 
+  gulp
+.src('assets/images/*.{jpg,png}')
+.pipe(imagewebp())
+.pipe(gulp.dest('dist/images'))
+);
 });
 
 
-// minify js
+// Transpile, concatenate and minify scripts
 gulp.task("js", () => {
-        return gulp
-  .src('./js/*.js') // change to your source directory
-    .pipe(terser())
-    .pipe(gulp.dest('dist/js')); // change to your final/public directory
+  return (
+    gulp
+      .src([
+        'assets/js/jquery-3.6.0.min.js',
+        'assets/js/popper.min.js',
+        'assets/js/bootstrap.min.js',
+        'assets/js/general.js'
+      ])
+      // folder only, filename is specified in webpack config
+      .pipe(concat('app.js'))
+      .pipe(terser())
+      .pipe(gulp.dest("dist/js"))
+      .pipe(browsersync.stream())
+      .pipe(livereload())
+  );
 });
 
-// //watchtask
-// function watchTask(){
-//   watch('./scss/*.scss', compilescss); // change to your source directory
-//   watch('./js/*.js', jsmin); // change to your source directory
-//   watch('./images/*', optimizeimg); // change to your source directory
-//   watch('dist/images/*.{jpg,png}', webpImage); // change to your source directory
-// }
+gulp.task("default", gulp.series("css", "js", "webfonts", "webpImage", "browser-sync", () => {
+  livereload.listen();
+  gulp.watch(["assets/scss/**/*"], gulp.series("css"));
+  gulp.watch(["assets/js/**/*"], gulp.series("js"));
+  gulp.watch(["assets/scss/vendor/fontawesome/webfonts/*"], gulp.series("webfonts"));
+  gulp.watch('assets/images/*.{jpg,png}', gulp.series("webpImage")); 
+}));
 
 
-// // Default Gulp task 
-// exports.default = series(
-//   compilescss,
-//   jsmin,
-//   optimizeimg,
-//   webpImage,
-//   watchTask
-// );
 
-gulp.task("default", gulp.series( "css", "js", "optimizeimg", "webpImage", () => {
-    gulp.watch("./scss/**/*", gulp.series("css")); // change to your source directory
-    gulp.watch('./js/**/*', gulp.series("js")); // change to your source directory
-    gulp.watch('./images/*', gulp.series("optimizeimg")); // change to your source directory
-    gulp.watch('dist/images/*.{jpg,png}', gulp.series("webpImage")); // change to your source directory
-  }));
-  
+
+
